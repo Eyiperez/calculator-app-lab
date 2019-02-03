d8666fe03eda7507265c0b2e34b3966c7144f0c5
Van Tran <vantran@pursuit.org>
2019-02-03 16:59:29 -0500

    completed buttons/display

--- calculator-app/src/App.js
+++ calculator-app/src/App.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
+import Button from './components/Button';
+import Display from './components/display';
+import './index.css';
 
 
 class App extends Component {
+
   render() {
     return (
-    <div>
-      <h1>Hello, Wrold!</h1>
-    </div>
+      <>
+      <div className='holder'>
+        <div className='calculator'>
+          <div className='row'>
+            <Display/>
+            <Button/>
+          </div>
+        </div>
+      </div>
+      </>
     );
   }
 }
+
+
 
 export default App;

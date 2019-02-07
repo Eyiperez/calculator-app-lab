ae4c6b50b3bf45bfb57c9e85e1081491261b3ef5
Van Tran <vantran@pursuit.org>
2019-02-06 21:16:34 -0500

    Working Buttons

--- calculator-app/src/App.js
+++ calculator-app/src/App.js
@@ -1,25 +1,58 @@
 import React, { Component } from 'react';
-import Button from './components/Button';
 import Display from './components/display';
 import './index.css';
+import Button from './container/Button';
 
 
 class App extends Component {
+ 
+  constructor(props) {
+    super(props);
+    this.state = {
 
-  render() {
-    return (
-      <>
-      <div className='holder'>
-        <div className='calculator'>
-          <div className='row'>
-            <Display/>
-            <Button/>
-          </div>
-        </div>
-      </div>
-      </>
-    );
+      displayValue: '0',
+      previousValue: null,
+      operation: null,
+      waitingForNewValue: false
+
+    }
   }
+handleNumClick = (e) => {
+console.log(e.target.value)
+this.setState({displayValue : e.target.value});
+
+}
+
+  render(){
+    return (
+    
+    <>
+    <div className = 'row'> 
+    <div class="col-12 inputview">{this.state.displayValue}</div>
+    <button class="button col-3" onClick={this.handleNumClick} value='0'>AC</button>
+    <button class="button col-3">%</button>
+    <button class="button col-3">±</button>
+    <button class="button col-3 orange">÷</button>
+    <button class="button col-3" onClick={this.handleNumClick} value='7' >7</button>
+    <button class="button col-3" onClick={this.handleNumClick} value='8' >8</button>
+    <button class="button col-3" onClick={this.handleNumClick} value='9' >9</button>
+    <button class="button col-3 orange">x</button>
+    <button class="button col-3" onClick={this.handleNumClick} value='4' >4</button>
+    <button class="button col-3" onClick={this.handleNumClick} value='5' >5</button>
+    <button class="button col-3" onClick={this.handleNumClick} value='6' >6</button>
+    <button class="button col-3 orange">-</button>
+    <button class="button col-3" value= '1' onClick={this.handleNumClick}>1</button>
+    <button class="button col-3" onClick={this.handleNumClick} value='2' >2</button>
+    <button class="button col-3" onClick={this.handleNumClick} value='3' >3</button>
+    <button class="button col-3 orange">+</button>
+    <button class="button col-6" onClick={this.handleNumClick} value='0' >0</button>
+    <button class="button col-3" onClick={this.handleNumClick} value='.' >.</button>
+    <button class="button col-3 orange">=</button>
+    </div>
+    </>
+
+)
+}
 }
 
 
---
+++ calculator-app/src/container/Button.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import '../index.css';
+//import Display from '../components/display';
+
+class Button extends Component  {
+    constructor(props) {
+        super(props);
+        this.state = {
+    
+          displayValue: '0',
+          previousValue: null,
+          operation: null,
+          waitingForNewValue: false
+    
+        }
+      }
+handleNumClick = (e) => {
+  console.log(e.target.value)
+this.setState({displayValue : e.target.value});
+
+}
+
+      render(){
+        return (
+        <>
+        <button class="button col-3" onClick={this.handleNumClick} value='AC' >AC</button>
+        <button class="button col-3">%</button>
+        <button class="button col-3">±</button>
+        <button class="button col-3 orange">÷</button>
+        <button class="button col-3" onClick={this.handleNumClick} value='7' >7</button>
+        <button class="button col-3" onClick={this.handleNumClick} value='8' >8</button>
+        <button class="button col-3">9</button>
+        <button class="button col-3 orange">x</button>
+        <button class="button col-3">4</button>
+        <button class="button col-3">5</button>
+        <button class="button col-3">6</button>
+        <button class="button col-3 orange">-</button>
+        <button class="button col-3" value= '1' onClick={this.handleNumClick}>1</button>
+        <button class="button col-3">2</button>
+        <button class="button col-3">3</button>
+        <button class="button col-3 orange">+</button>
+        <button class="button col-6">0</button>
+        <button class="button col-3">.</button>
+        <button class="button col-3 orange">=</button>
+        </>
+    
+    )
+ }
+}  
+
+
+
+
+export default Button;

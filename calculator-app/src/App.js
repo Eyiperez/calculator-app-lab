352066ee02ef2cca562d9bfc688c31fa4bf1ddd0
Erika Perez <erikaperezvalle@pursuit.org>
2019-02-07 23:31:11 -0500

    managed to get the addition operation working, not perfectly because concat is not working for new value, but when you click equals you do get the sum back. the period is also working but as well not properly

--- calculator-app/src/App.js
+++ calculator-app/src/App.js
@@ -17,30 +17,56 @@ class App extends Component {
 
     }
   }
-  
+
   handleNumClick = (e) => {
     console.log(e.target.value)
-    if (this.state.displayValue === '0'){
+    if (this.state.displayValue === '0') {
       this.setState({ displayValue: e.target.value });
 
     }
-   else {
-    const newVal = this.state.displayValue.concat(e.target.value)
-    this.setState({ displayValue: newVal })
-   }
+    if (this.state.displayValue !== '0') {
+      const newVal = this.state.displayValue.concat(e.target.value)
+      this.setState({ displayValue: newVal })
+    }
+   if(this.state.waitingForNewValue === true) {
+      this.setState({ displayValue: e.target.value });
+    }
+    
 
   }
 
+  handlePeriodClick = (e) => {
+    const newVal = this.state.displayValue.concat(e.target.value)
+      this.setState({ displayValue: newVal })
+  }
+
   handleClearClick = (e) => {
-    if (this.state.previousValue === null){
+    if (this.state.previousValue === null) {
       this.setState({ displayValue: 0 })
+    }
+    if (this.state.previousValue !== null) {
+      this.setState({
+        displayValue: '0',
+        previousValue: null,
+        operation: null,
+        waitingForNewValue: false
+      })
     }
   }
 
   handleAddClick = (e) => {
-    
+    this.setState({ operation: 'add' });
+    this.setState({ waitingForNewValue: true });
+    this.setState({ previousValue: this.state.displayValue });
+    console.log(this.state)
   }
-  
+
+  handleEqualsClick = (e) => {
+    if (this.state.operation === 'add') {
+      const sum = parseInt(this.state.displayValue) + parseInt(this.state.previousValue);
+      this.setState({ displayValue: sum });
+    }
+  }
 
   render() {
     return (
@@ -49,26 +75,26 @@ class App extends Component {
         <div className='holder'>
           <div className='calculator'>
             <div className='row'>
-              <div class="col-12 inputview">{this.state.displayValue}</div>
-              <button class="button col-3" onClick={this.handleClearClick} value='0'>AC</button>
-              <button class="button col-3">%</button>
-              <button class="button col-3">±</button>
-              <button class="button col-3 orange">÷</button>
-              <button class="button col-3" onClick={this.handleNumClick} value='7' >7</button>
-              <button class="button col-3" onClick={this.handleNumClick} value='8' >8</button>
-              <button class="button col-3" onClick={this.handleNumClick} value='9' >9</button>
-              <button class="button col-3 orange">x</button>
-              <button class="button col-3" onClick={this.handleNumClick} value='4' >4</button>
-              <button class="button col-3" onClick={this.handleNumClick} value='5' >5</button>
-              <button class="button col-3" onClick={this.handleNumClick} value='6' >6</button>
-              <button class="button col-3 orange">-</button>
-              <button class="button col-3" value='1' onClick={this.handleNumClick}>1</button>
-              <button class="button col-3" onClick={this.handleNumClick} value='2' >2</button>
-              <button class="button col-3" onClick={this.handleNumClick} value='3' >3</button>
-              <button class="button col-3 orange">+</button>
-              <button class="button col-6" onClick={this.handleNumClick} value='0' >0</button>
-              <button class="button col-3" onClick={this.handleNumClick} value='.' >.</button>
-              <button class="button col-3 orange">=</button>
+              <div className="col-12 inputview">{this.state.displayValue}</div>
+              <button className="button col-3" onClick={this.handleClearClick} value='0'>AC</button>
+              <button className="button col-3">%</button>
+              <button className="button col-3">±</button>
+              <button className="button col-3 orange">÷</button>
+              <button className="button col-3" onClick={this.handleNumClick} value='7' >7</button>
+              <button className="button col-3" onClick={this.handleNumClick} value='8' >8</button>
+              <button className="button col-3" onClick={this.handleNumClick} value='9' >9</button>
+              <button className="button col-3 orange">x</button>
+              <button className="button col-3" onClick={this.handleNumClick} value='4' >4</button>
+              <button className="button col-3" onClick={this.handleNumClick} value='5' >5</button>
+              <button className="button col-3" onClick={this.handleNumClick} value='6' >6</button>
+              <button className="button col-3 orange">-</button>
+              <button className="button col-3" value='1' onClick={this.handleNumClick}>1</button>
+              <button className="button col-3" onClick={this.handleNumClick} value='2' >2</button>
+              <button className="button col-3" onClick={this.handleNumClick} value='3' >3</button>
+              <button className="button col-3 orange" onClick={this.handleAddClick}>+</button>
+              <button className="button col-6" onClick={this.handleNumClick} value='0' >0</button>
+              <button className="button col-3" onClick={this.handlePeriodClick} value='.' >.</button>
+              <button className="button col-3 orange" onClick={this.handleEqualsClick}>=</button>
             </div>
           </div>
         </div>
